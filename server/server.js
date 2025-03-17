b8ea32595f2ff7062355ceefc91a5f0722a84dda
shubham italiya <shubhamitaliya05@gmail.com>
2025-03-17 19:18:35 +0530

    Remove custom CORS headers middleware to streamline server configuration

--- server/server.js
+++ server/server.js
@@ -21,19 +21,6 @@ app.use(cors());
 // Basic middleware
 app.use(express.json());
 
-// Global headers middleware
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
-
 // MongoDB Connection
 // mongoose.connect(process.env.MONGO_URI, {
 //   useNewUrlParser: true,
